e89a08da60c7265ffde96a57c8f9168361824889
Vincenzo Filetti <vzfiletti@gmail.com>
2017-06-11 12:47:39 +0200

    Login user failed action improved

--- findyourbuddy/src/reducers/authReducer.js
+++ findyourbuddy/src/reducers/authReducer.js
@@ -1,6 +1,8 @@
 import { LOGIN_USER_START, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from '../actions/types';
 let initialState = {
-  user: null
+  user: null,
+  error: null,
+  isLoading: false
 }
 export default authReducer = (state = initialState, action) => {
   switch(action.type){
@@ -8,11 +10,14 @@ export default authReducer = (state = initialState, action) => {
 
       return {...state, isLoading: true}
     case LOGIN_USER_SUCCESS:
-
       console.log('Loggato correttamente')
       return {...state, user: action.payload, isLoading: false}
     case LOGIN_USER_FAIL:
-      return {...state, isLoading: false, error: action.payload.error}
+      console.log('login user fail')
+      return {
+        error: action.payload,
+        isLoading: false
+      }
     default:
       return state;
   }
