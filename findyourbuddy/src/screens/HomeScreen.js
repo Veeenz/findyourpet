12dfa16d6bb080e8b96efd25b7ac024f7f096316
Vincenzo Filetti <vzfiletti@gmail.com>
2017-06-11 13:49:34 +0200

    Map added and user permission asked

--- findyourbuddy/src/screens/HomeScreen.js
+++ findyourbuddy/src/screens/HomeScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import {connect} from 'react-redux';
-import { userProfileInformation } from '../actions/actions';
+import { userProfileInformation, setUserLocation } from '../actions/actions';
+import { MapView, Permissions, Location } from 'expo';
 
 const mapStateToProps = state => ({
     user: state.user
+})
+const mapDispatchToProps = dispatch => ({
+  userProfileInformation: () => dispatch(userProfileInformation()),
+  setUserLocation:(coordinates) => dispatch(setUserLocation(coordinates)),
 })
 
 class HomeScreen extends React.Component{
@@ -13,18 +18,50 @@ class HomeScreen extends React.Component{
   }
   componentWillMount(){
     this.props.userProfileInformation()
+    this.retrieveUserLocation()
 
   }
-  render(){
 
+  retrieveUserLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({ errorMessage: 'Permesso negato'});
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.props.setUserLocation(location.coords);
+  }
+
+  render(){
+    const { width, height } = Dimensions.get('window');
+    const { latitude, longitude } = this.props.user
     return(
       <View>
-        <Text>
-          Hello, {this.props.user.email}
-        </Text>
+
+        <MapView
+          style={{ width, height: height-200 }}
+          region={{
+            latitude: this.props.user.latitude,
+            longitude: this.props.user.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+        <MapView.Marker
+          coordinate={{
+            latitude: this.props.user.latitude || 10.10293,
+            longitude: this.props.user.longitude || 10.0293, 
+          }}
+          title='Sei qui'
+        />
+        </MapView>
+        <Text> Hello {this.props.user.email}</Text>
+        <Text> Coordinate longitude {this.props.user.longitude}</Text>
+        <Text> Coordinate latitude {this.props.user.latitude}</Text>
       </View>
+
     );
   }
 }
 
-export default connect(mapStateToProps, { userProfileInformation })(HomeScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
