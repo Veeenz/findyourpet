9be58237a6df125797eca288d656b985830676c1
Vincenzo Filetti <vzfiletti@gmail.com>
2017-06-11 13:48:59 +0200

    userReducer updated with new action

--- findyourbuddy/src/reducers/userReducer.js
+++ findyourbuddy/src/reducers/userReducer.js
@@ -1,5 +1,11 @@
-import { USER_PROFILE_INFORMATION } from '../actions/types';
-export default userReducer = (state = {}, action) => {
+import { USER_PROFILE_INFORMATION, USER_SET_LOCATION } from '../actions/types';
+let initialState = {
+  latitude: 15.102032,
+  longitude: 36.019283,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+}
+export default userReducer = (state = initialState, action) => {
   switch(action.type){
     case USER_PROFILE_INFORMATION:
     console.log('@@@@@@@@@@ START ACTION PAYLOAD ')
@@ -8,6 +14,15 @@ export default userReducer = (state = {}, action) => {
 
       return {
         email: action.payload.email
+      }
+
+    case USER_SET_LOCATION:
+      return{
+        ...state,
+        longitude: action.payload.longitude,
+        latitude: action.payload.latitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
       }
     default:
       return state;
