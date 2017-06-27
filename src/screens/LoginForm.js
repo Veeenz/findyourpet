0523bd0fb761aa148f2b8c7434c28b069b089ba3
Vincenzo Filetti <vzfiletti@gmail.com>
2017-06-27 18:24:49 +0200

    Fixed indentation on LoginForm

--- src/screens/LoginForm.js
+++ src/screens/LoginForm.js
@@ -7,146 +7,146 @@ import { loginUser } from '../actions/actions';
 import ErrorCard from '../components/ErrorCard';
 
 const mapStateToProps = state => ({
-  auth: state.auth
+    auth: state.auth
 })
 
 class LoginForm extends Component {
-  state = {
-    email: '',
-    password: '',
-    loginLoad: false
-  }
+    state = {
+        email: '',
+        password: '',
+        loginLoad: false
+    }
 
-  handleAuthenticationError = () => {
-    if(this.props.auth.error)
-    return (
-      <ErrorCard>
-        {this.props.auth.error.message}
-      </ErrorCard>
-    )
+    handleAuthenticationError = () => {
+        if(this.props.auth.error)
+        return (
+            <ErrorCard>
+                {this.props.auth.error.message}
+            </ErrorCard>
+        )
 
-  }
+    }
 
-  handleLoginClickAuth = () => {
+    handleLoginClickAuth = () => {
 
-      this.props.loginUser(
-        {
-          email: 'a@a.it',
-          password: 'aaaaaa',
-          navigateTo: (screen) => this.props.navigation.navigate(screen)
-        }
-      )
-
-  }
-
-  componentWillMount() {
-    console.log("Componente login montato")
-   console.log(this.state)
-
-  }
-
-  render(){
-    if(this.props.auth.isLoading)
-      return(
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text></Text>
-            <Spinner color='blue' />
-        </View>
-      );
-    if( this.state.loginLoad)
-    return(
-      <Container>
-        <Content>
-          <Card>
-            <CardItem>
-              <Item stackedLabel style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                  <Button style={{marginTop:20}} block primary onPress={() =>  this.props.navigation.navigate("Create") }>
-                    <Text>DEBUG CreateFind</Text>
-                  </Button>
-              </Item>
-            </CardItem>
-            <CardItem>
-              <Item stackedLabel style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                  <Button style={{marginTop:20}} block primary onPress={() =>   this.setState({loginLoad:false }) }>
-                    <Text>LOGOUT </Text>
-                  </Button>
-              </Item>
-            </CardItem>
-
-                    <Text>LOGIN EFFETTUATO</Text>
-          </Card>
-        </Content>
-      </Container>
-
-
-
-
-    );
-    return(
-
-      <Container>
-        <Content>
-          <Card>
-
-
-          {this.handleAuthenticationError()}
-          <CardItem>
-          <Item stackedLabel style={{ flex:1 }}>
-              <Label>Email</Label>
-              <Input onChangeText={(email) => this.setState({email})} />
-          </Item>
-        </CardItem>
-        <CardItem>
-          <Item stackedLabel style={{ flex:1 }}>
-              <Label>Password</Label>
-              <Input onChangeText={(password) => this.setState({password})}/>
-          </Item>
-        </CardItem>
-        <CardItem>
-          <Item stackedLabel style={{ flex:1 }}>
-            <Button block primary onPress={() => this.props.loginUser(
-              {
-                email: this.state.email,
-                password: this.state.password,
+        this.props.loginUser(
+            {
+                email: 'a@a.it',
+                password: 'aaaaaa',
                 navigateTo: (screen) => this.props.navigation.navigate(screen)
-              }
-            )}>
-              <Text>Log in</Text>
-            </Button>
-          </Item>
-      </CardItem>
-      <CardItem>
-        <Item stackedLabel style={{ flex:1 }}>
-            <Button style={{marginTop:20}} block primary onPress={() => { this.setState({loginLoad:true },() => {console.log(this.state)}) }}>
-              <Text>DEBUG Login ever true</Text>
-            </Button>
-        </Item>
-      </CardItem>
-      <CardItem>
-        <Item stackedLabel style={{ flex:1 }}>
-            <Button style={{marginTop:20}} block primary onPress={() =>  this.props.navigation.navigate("Create") }>
-              <Text>DEBUG CreateFind</Text>
-            </Button>
-        </Item>
-      </CardItem>
-          </Card>
-          </Content>
+            }
+        )
+
+    }
+
+    componentWillMount() {
+        console.log("Componente login montato")
+        console.log(this.state)
+
+    }
+
+    render(){
+        if(this.props.auth.isLoading)
+        return(
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text></Text>
+                <Spinner color='blue' />
+            </View>
+        );
+        if( this.state.loginLoad)
+        return(
+            <Container>
+                <Content>
+                    <Card>
+                        <CardItem>
+                            <Item stackedLabel style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                                <Button style={{marginTop:20}} block primary onPress={() =>  this.props.navigation.navigate("Create") }>
+                                    <Text>DEBUG CreateFind</Text>
+                                </Button>
+                            </Item>
+                        </CardItem>
+                        <CardItem>
+                            <Item stackedLabel style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                                <Button style={{marginTop:20}} block primary onPress={() =>   this.setState({loginLoad:false }) }>
+                                    <Text>LOGOUT </Text>
+                                </Button>
+                            </Item>
+                        </CardItem>
+
+                        <Text>LOGIN EFFETTUATO</Text>
+                    </Card>
+                </Content>
+            </Container>
+
+
+
+
+        );
+        return(
+
+            <Container>
+                <Content>
+                    <Card>
+
+
+                        {this.handleAuthenticationError()}
+                        <CardItem>
+                            <Item stackedLabel style={{ flex:1 }}>
+                                <Label>Email</Label>
+                                <Input onChangeText={(email) => this.setState({email})} />
+                            </Item>
+                        </CardItem>
+                        <CardItem>
+                            <Item stackedLabel style={{ flex:1 }}>
+                                <Label>Password</Label>
+                                <Input onChangeText={(password) => this.setState({password})}/>
+                            </Item>
+                        </CardItem>
+                        <CardItem>
+                            <Item stackedLabel style={{ flex:1 }}>
+                                <Button block primary onPress={() => this.props.loginUser(
+                                    {
+                                        email: this.state.email,
+                                        password: this.state.password,
+                                        navigateTo: (screen) => this.props.navigation.navigate(screen)
+                                    }
+                                )}>
+                                <Text>Log in</Text>
+                            </Button>
+                        </Item>
+                    </CardItem>
+                    <CardItem>
+                        <Item stackedLabel style={{ flex:1 }}>
+                            <Button style={{marginTop:20}} block primary onPress={() => { this.setState({loginLoad:true },() => {console.log(this.state)}) }}>
+                                <Text>DEBUG Login ever true</Text>
+                            </Button>
+                        </Item>
+                    </CardItem>
+                    <CardItem>
+                        <Item stackedLabel style={{ flex:1 }}>
+                            <Button style={{marginTop:20}} block primary onPress={() =>  this.props.navigation.navigate("Create") }>
+                                <Text>DEBUG CreateFind</Text>
+                            </Button>
+                        </Item>
+                    </CardItem>
+                </Card>
+            </Content>
         </Container>
     );
-  }
+}
 };
 
 LoginForm.navigationOptions = ({ navigation }) => ({
-  title: "Login"
-  // headerLeft: <Button title="Login" onPress={() => navigation.goBack()} />,
+    title: "Login"
+    // headerLeft: <Button title="Login" onPress={() => navigation.goBack()} />,
 });
 
 const style = StyleSheet.create({
-  login:{
-    width:'80%',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    login:{
+        width:'80%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
 
 export default connect(mapStateToProps, { loginUser })(LoginForm);
