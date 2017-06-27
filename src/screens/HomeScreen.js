8f2b3a6c080fc18551bda8cf46fbc53a922503c9
Vincenzo Filetti <vzfiletti@gmail.com>
2017-06-27 18:24:06 +0200

    Fixed indentation and added Marker Loading

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ import firebase from 'firebase';
 
 const mapStateToProps = state => {
     const petList = Object.keys(state.pet.petList).map(id => {
-      return { ...state.pet.petList[id] }
+        return { ...state.pet.petList[id] }
     });
     return{
         user: state.user,
@@ -17,97 +17,88 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-  userProfileInformation: () => dispatch(userProfileInformation()),
-  setUserLocation:(coordinates) => dispatch(setUserLocation(coordinates)),
-  setUserMarker:(coordinates) => dispatch(setUserMarker(coordinates)),
-  logoutUser: () => dispatch(logoutUser()),
-  findListFetch: () => dispatch(findListFetch())
+    userProfileInformation: () => dispatch(userProfileInformation()),
+    setUserLocation:(coordinates) => dispatch(setUserLocation(coordinates)),
+    setUserMarker:(coordinates) => dispatch(setUserMarker(coordinates)),
+    logoutUser: () => dispatch(logoutUser()),
+    findListFetch: () => dispatch(findListFetch())
 })
 
 class HomeScreen extends React.Component{
-  constructor(props){
-    super(props);
-  }
-  componentWillMount(){
-    //this.props.userProfileInformation()
-    this.retrieveUserLocation()
-    this.props.findListFetch()
-  }
-
-
-
-  retrieveUserLocation = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({ errorMessage: 'Permesso negato'});
+    constructor(props){
+        super(props);
+    }
+    componentWillMount(){
+        //this.props.userProfileInformation()
+        this.retrieveUserLocation()
+        this.props.findListFetch()
     }
 
-    let location = await Location.getCurrentPositionAsync({});
-    this.props.setUserLocation(location.coords);
-  }
-
-  handleClickButton = () => {
-    this.props.findListFetch();
-  }
-
-  markerRender = () => {
-      return Object.keys(this.props.pet).map((key) => { // TODO: Need to find a better way to manage this
-          return (<MapView.Marker
-                      key={key}
-                      coordinate={{
-                          latitude: this.props.pet[key].latitudeMarker,
-                          longitude: this.props.pet[key].longitudeMarker
-                      }}
-                  />)
-      })
-  }
-
-  render(){
-    const { width, height } = Dimensions.get('window');
-    const { latitude, longitude, longitudeMarker, latidudeMarker } = this.props.user
-    return(
-      <Container>
-        <Content
-          padder= {false}
-          >
-        <Card >
-          <Header>
-             <Body>
-                 <Title>Maps</Title>
-             </Body>
-         </Header>
-          <CardItem>
-        <View>
-          <Button onPress={() => this.handleClickButton()}><Text>Hello</Text></Button>
-          <MapView
-            style={{ width, height: height }}
-            showsUserLocation={true}
-            region={{
-              latitude: this.props.user.latitude,
-              longitude: this.props.user.longitude,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-
-          >
-          </MapView>
-          </View>
-
-        </CardItem>
-          </Card>
-        </Content>
-    </Container>
 
 
+    retrieveUserLocation = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({ errorMessage: 'Permesso negato'});
+        }
 
-    );
-  }
-}
+        let location = await Location.getCurrentPositionAsync({});
+        this.props.setUserLocation(location.coords);
+    }
 
+    handleClickButton = () => {
+        this.props.findListFetch();
+    }
 
+    markerRender = () => {
+        return Object.keys(this.props.pet).map((key) => { // TODO: Need to find a better way to manage this
+            return (<MapView.Marker
+                key={key}
+                coordinate={{
+                    latitude: this.props.pet[key].latitudeMarker,
+                    longitude: this.props.pet[key].longitudeMarker
+                }}
+            />)
+        })
+    }
 
+    render(){
+        const { width, height } = Dimensions.get('window');
+        const { latitude, longitude, longitudeMarker, latidudeMarker } = this.props.user
+        return(
+            <Container>
+                <Content
+                    padder= {false}
+                    >
+                        <Card >
+                            <Header>
+                                <Body>
+                                    <Title>Maps</Title>
+                                </Body>
+                            </Header>
+                            <CardItem>
+                                <View>
+                                    <Button onPress={() => this.handleClickButton()}><Text>Hello</Text></Button>
+                                    <MapView
+                                        style={{ width, height: height }}
+                                        showsUserLocation={true}
+                                        region={{
+                                            latitude: this.props.user.latitude,
+                                            longitude: this.props.user.longitude,
+                                            latitudeDelta: 0.0922,
+                                            longitudeDelta: 0.0421,
+                                        }}
 
+                                        >
+                                        {this.markerRender()}
+                                        </MapView>
+                                    </View>
 
-
-
+                                </CardItem>
+                            </Card>
+                        </Content>
+                    </Container>
+                );
+            }
+        }
 export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
